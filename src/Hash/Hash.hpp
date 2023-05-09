fa8b9ff089add155fd57c21bf09ea2c446a3b395
Eduardo Savian de Oliveira <eduardosaviandeoliveira@proton.me>
2023-05-09 19:29:23 -0300

    Added structs

--- src/Hash/Hash.hpp
+++ src/Hash/Hash.hpp
@@ -4,4 +4,34 @@
 #include <iostream>
 #include <string>
 
+struct Autor {
+    int id;
+    std::string name;
+};
+
+
+struct Publisher {
+    int id;
+    std::string name;
+};
+
+struct Book {
+    std::string name;
+    int id;
+    Autor autor;
+    Publisher publisher;
+};
+
+struct Magazine {
+    int id;
+    std::string name;
+    Autor autor;
+    Publisher publisher;
+};
+
+struct User {
+    int id;
+    std::string name;
+};
+
 #endif
